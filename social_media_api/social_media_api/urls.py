9265a91acc3814a99cb5b7f24f5a2a901fb98aa6
Guandaru-Kabugi <kabugi96@gmail.com>
2024-09-21 20:16:29 +0300

    another change to docs

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -17,34 +17,34 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from django.http import HttpResponseRedirect
+# from drf_yasg.views import get_schema_view
+# from django.http import HttpResponseRedirect
 from django.urls import reverse
 from drf_yasg import openapi
-def redirect_to_swagger(request):
-    return HttpResponseRedirect(reverse('schema-swagger-ui'))
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Social Media API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.example.com/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+# def redirect_to_swagger(request):
+#     return HttpResponseRedirect(reverse('schema-swagger-ui'))
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Social Media API",
+#         default_version='v1',
+#         description="Test description",
+#         terms_of_service="https://www.example.com/terms/",
+#         contact=openapi.Contact(email="contact@example.com"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 
 urlpatterns = [
     path('secret-admin/', admin.site.urls),
     path('account/',include('accounts.urls')),
     path('api/',include('posts.urls')),
-    path('user',include('notifications.urls')),
-    path('', redirect_to_swagger),
+    path('user/',include('notifications.urls')),
+    # path('', redirect_to_swagger),
 
     # drf-yasg endpoints for swagger and redoc
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
